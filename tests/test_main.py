ac25867bb545a400050ae0bd52740b4ce66140da
Kendell009 <salma09iqbal@gmail.com>
2025-10-28 23:27:23 +0100

    added subtract function in test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,15 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add_function():
     assert add(2,3) == 5
     assert add(6,6) == 12
     assert add(4,4) == 8
-    assert add(0,0) == 2
+    assert add(0,0) == 0
+
+
+def test_subtract_function():
+    assert subtract(6,3) == 3
+    assert subtract(120, 60) == 60
+    assert subtract(10,5) == 5
 
 
